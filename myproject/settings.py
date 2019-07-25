cbfdcbc1e480e12792bba51413a544747232369c
Anna Pershyna <ny4koo@gmail.com>
2019-07-25 22:16:24 +0200

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -143,13 +143,13 @@ FILE_UPLOAD_PERMISSIONS = 0o644
 DATABASES = {
     "default": {
         # Add "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
-        "ENGINE": "django.db.backends.",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
         # DB name or path to database file if using sqlite3.
-        "NAME": "",
+        "NAME": "myproject",
         # Not used with sqlite3.
-        "USER": "",
+        "USER": "myuser",
         # Not used with sqlite3.
-        "PASSWORD": "",
+        "PASSWORD": "mypassword",
         # Set to empty string for localhost. Not used with sqlite3.
         "HOST": "",
         # Set to empty string for default. Not used with sqlite3.
